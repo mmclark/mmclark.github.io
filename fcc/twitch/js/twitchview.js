382d02c65c078811a8a0606a1fc892dfd51e8da3
Miles Clark <mmc@obispo.com>
2017-03-27 12:07:55 -0700

    List featured streams as cards - first pass

--- fcc/twitch/js/twitchview.js
+++ fcc/twitch/js/twitchview.js
@@ -2,14 +2,42 @@ var TWITCHY = TWITCHY || {};
 
 
 TWITCHY.Main = (function() {
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  var apiurl = 'https://wind-bow.gomix.me/twitch-api';  // channels/freecodecamp/';
+  var cardtemplate =
+      '<div class="col-sm-6"> \
+          <div class="card"> \
+            <div class="card-block">\
+              <h3 class="card-title">{{STREAMTITLE}}</h3>\
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>\
+              <a href="#" class="btn btn-primary">Go somewhere</a>\
+            </div>\
+          </div>\
+        </div>';
 
-    function hello() {
-        console.log('hello');
-    }
+  function hello() {
+    console.log('hello from twitchy');
+  }
 
-    return {
-        hello: hello
-    }
+  function listStreams() {
+    console.log('listStreams ENTER');
+    var url = proxy + apiurl + '/streams/featured';
+    $.getJSON(url, function(data) {
+      //console.log(data);
+      var streams = $('#streams');
+      console.log('streams:', streams);
+      for (var i=0; i < data.featured.length; i++) {
+	$(cardtemplate.replace("{{STREAMTITLE}}", data.featured[i].title)).appendTo(streams);
+      }
+    }).fail(function() {
+      console.log('call to', url, 'failed');
+    });
+  }
+
+  return {
+    hello: hello,
+    listStreams: listStreams
+  };
 
 })();
 
@@ -38,5 +66,4 @@ var oldtwitchy = {
 };
 
 TWITCHY.Main.hello();
-
-
+TWITCHY.Main.listStreams();
